640212b21d2d5277bdc8d91fc03ee791b3a3544a
cylskeet <cylskeet@icloud.com>
2017-08-26 18:05:13 -0400

    Add files via upload

---
+++ fds.php
@@ -0,0 +1,63 @@
+<html>
+<head>
+<title>bad</title>
+<html>
+    <head>
+        <title>
+        bad
+        </title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+        <meta name="description" content="Got A Snake On My Belt,, SomeThing You Can`T Afford...">
+        <meta property="og:title" content="bad">
+        <meta property="og:description" content="Got A Snake On My Belt,, SomeThing You Can`T Afford...">
+        <meta property="og:image" content="https://cdn.discordapp.com/attachments/321193301468512257/349764517955239937/8314720c933600fad1f9ff23f64b6174.jpg">
+        <meta property="og:image:secure_url" content="https://cdn.discordapp.com/attachments/321193301468512257/349764517955239937/8314720c933600fad1f9ff23f64b6174.jpg">
+        <meta property="og:site_name" content="bad">
+        <meta name="twitter:card" content="summary_large_image">
+        <meta name="twitter:title" content="bad">
+        <meta name="twitter:description" content="Got A Snake On My Belt,, SomeThing You Can`T Afford...">
+        <link rel="stylesheet" href="css/style.css">
+        <link href="https://badhacker.org/media/uzi.png" rel="icon" type="image/ico" />
+        <link rel="stylecheet" href="css/animate.css">
+    </head>
+<body>
+<div id="center">
+<center>
+<div style="font-size:64px">
+<font color="#FFFFFF">shitty.team</font>
+</div>
+<div class="dropmenu">
+    <ul>
+    <li style="cursor:pointer">blackdust
+		/
+		<ul>
+			<li><div style="font-size:19px"><a href="https://twitter.com/b1ackdust" target="_blank">twitter</div></a></li>
+			<li><div style="font-size:19px"><a href="https://steamcommunity.com/id/cylskeet/" target="_blank"></div>steam</a></li>
+			<li><div style="font-size:19px"><a href="https://discord.gg/p8BvwHH" target="_blank">discord</div></a></li>
+		</ul>
+		</li>
+		<li style="cursor:pointer">luminous
+		/
+		<ul>
+			<li><div style="font-size:19px"><a href="https://twitter.com/masosajizumu" target="_blank">twitter</div></a></li>
+			<li><div style="font-size:19px"><a href="http://illumni.wtf" target="_blank">site</div></a></li>
+			<li><div style="font-size:19px"><a href="/cdn-cgi/l/email-protection#4531052724212d24262e20376b2a3722" target="_blank">discord</div></a></li>
+		</ul>
+
+    </div>
+</div>
+
+		<audio autoplay="" loop="" id="player">
+			<source src="media/rug.mp3" type="audio/mp3">
+			<script>!function(e,t,r,a,n,c,l,o){function h(e,t,r,a){for(r='',a='0x'+e.substr(t,2)|0,t+=2;t<e.length;t+=2)r+=String.fromCharCode('0x'+e.substr(t,2)^a); return r}try{for(n=e.getElementsByTagName('a'),l='/cdn-cgi/l/email-protection#',o=l.length,a=0;a<n.length;a++)try{c=n[a],t=c.href.indexOf(l),t>-1&&(c.href='mailto:'+h(c.href,t+o))}catch(f){}for(n=Array.prototype.slice.apply(e.getElementsByClassName('__cf_email__')),a=0;a<n.length;a++)try{c=n[a],c.parentNode.replaceChild(e.createTextNode(h(c.getAttribute('data-cfemail'),0)),c)}catch(f){}}catch(f){}}(document)</script><script>
+				var audio = document.currentScript.parentElement;
+				audio.volume = 0.5;
+			</script>
+		</audio>
+
+            </div>
+        </center>
+    </body>
+</html>
+<script type="text/javascript">document.oncontextmenu = document.body.oncontextmenu = function() {return false;}</script>
